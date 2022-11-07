418955022b32be8311cd4dbe22a16fe38845131e
Vaishnavi Vinayak Acharya <74627582+vaishnavi1505@users.noreply.github.com>
2022-11-07 11:20:06 +0100

    Add files via upload

---
+++ renamefile.py
@@ -0,0 +1,24 @@
+import os
+
+folder = r'D:\OFFIS\Green\Results'
+count = 1
+# count increase by 1 in each iteration
+# iterate all files from a directory
+for file_name in os.listdir(folder):
+    # Construct old file name
+
+     source = folder +'\\'+ file_name
+
+    # Adding the count to the new file name and extension
+     destination = folder + '\\' + str(count) + '.xlsx'
+     print(destination)
+
+    # Renaming the file
+     os.rename(source, destination)
+     count += 1
+print('All Files Renamed')
+
+print('New Names are')
+# verify the result
+res = os.listdir(folder)
+print(res)
